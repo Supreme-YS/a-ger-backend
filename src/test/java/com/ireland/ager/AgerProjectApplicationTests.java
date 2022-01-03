90548bd10fcfcdb4ffaf65c3ba7dac2f47633beb
dhkstnaos <dhkstnaos@gmail.com>
2022-01-03 16:25:43 +0900

    ADD develop setting

---
+++ src/test/java/com/ireland/ager/AgerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ireland.ager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
