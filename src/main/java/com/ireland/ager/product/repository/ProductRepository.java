74f0e1a212b1948f917ac1ed0e6060b2c3437932
jaeho <jhdl0157@naver.com>
2022-01-18 22:10:16 +0900

    Fix product.repository

--- src/main/java/com/ireland/ager/product/repository/ProductRepository.java
+++ src/main/java/com/ireland/ager/product/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.ireland.ager.product.entity.Category;
 import com.ireland.ager.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findProductsByCategory(Pageable pageable, Category category);
-
+    @EntityGraph(attributePaths = {"account","urlList"})
     Page<Product> findProductsByProductIdIsLessThanOrderByCreatedAtDesc(Long productId, Pageable pageRequest);
 }
