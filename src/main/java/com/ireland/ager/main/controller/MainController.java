d564948c2465f5311b4b4ebcf2f30810b90f1741
dhkstnaos <dhkstnaos@gmail.com>
2022-02-10 17:07:10 +0900

    Add 최근 검색어

--- src/main/java/com/ireland/ager/main/controller/MainController.java
+++ src/main/java/com/ireland/ager/main/controller/MainController.java
@@ -6,6 +6,7 @@ import com.ireland.ager.board.service.BoardServiceImpl;
 import com.ireland.ager.main.common.ListResult;
 import com.ireland.ager.main.common.SliceResult;
 import com.ireland.ager.main.common.service.ResponseService;
+import com.ireland.ager.main.service.RedisService;
 import com.ireland.ager.product.dto.response.ProductResponse;
 import com.ireland.ager.product.dto.response.ProductThumbResponse;
 import com.ireland.ager.product.entity.Category;
@@ -16,10 +17,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -30,20 +28,31 @@ public class MainController {
     private final ProductServiceImpl productService;
     private final BoardServiceImpl boardService;
     private final ResponseService responseService;
-
+    private final RedisService redisService;
     @GetMapping("/api/product/search")
     public ResponseEntity<SliceResult<ProductThumbResponse>> searchAllProducts(
-            @RequestParam(value = "category",required = false)Category category
+            @RequestHeader("Authorization") String accessToken
+            ,@RequestParam(value = "category",required = false)Category category
             ,@RequestParam(value = "keyword",required = false) String keyword
             ,Pageable pageable) {
+        String[] splitToken = accessToken.split(" ");
+        redisService.postKeyword(splitToken[1],keyword);
         return new ResponseEntity<>(responseService.getSliceResult(
                 productService.findProductAllByCreatedAtDesc(category,keyword,pageable)), HttpStatus.OK);
     }
     @GetMapping("/api/board/search")
-    public ResponseEntity<SliceResult<BoardSummaryResponse>> searchAllProducts(
-            @RequestParam(value = "keyword",required = false) String keyword
+    public ResponseEntity<SliceResult<BoardSummaryResponse>> searchAllBoards(
+            @RequestHeader("Authorization") String accessToken
+            ,@RequestParam(value = "keyword",required = false) String keyword
             ,Pageable pageable) {
         return new ResponseEntity<>(responseService.getSliceResult(
                 boardService.findBoardAllByCreatedAtDesc(keyword,pageable)), HttpStatus.OK);
+    }
+    @GetMapping("/api/keyword")
+    public ResponseEntity<ListResult<String>> searchAccountKeywords(
+            @RequestHeader("Authorization") String accessToken) {
+        String[] splitToken = accessToken.split(" ");
+        return new ResponseEntity<>(responseService.getListResult(
+                redisService.getSearchList(splitToken[1])),HttpStatus.OK);
     }
 }
